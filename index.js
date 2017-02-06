950f3f4d85a6540ba836367647761d4fded5673e
Thiago Lagden <lagden@gmail.com>
2017-02-06 15:40:13 -0200

    ulala

---
+++ index.js
@@ -0,0 +1,59 @@
+const {spawn} = require('child_process')
+const {join} = require('path')
+const qs = require('querystring')
+const url = require('url')
+const {send} = require('micro')
+const microCors = require('micro-cors')
+const compress = require('micro-compress')
+const streamToPromise = require('stream-to-promise')
+const got = require('got')
+
+// require('now-logs')('chuchu-blz')
+
+const slack = require('./_slack.json')
+const bin = join(__dirname, 'node_modules', 'lagden-stock-quote-cli', 'cli.js')
+const cors = microCors({allowMethods: ['POST']})
+
+function check(data) {
+	if (data && data.token && data.token === slack.token) {
+		return Promise.resolve()
+	}
+	return Promise.reject(new Error('Token inválido'))
+}
+
+async function delayed(data) {
+	const infobuf = await streamToPromise(spawn(bin, [data.text]).stdout)
+	const info = infobuf.toString('utf8')
+	got
+		.post(data.response_url, {
+			headers: {
+				'content-type': 'application/json'
+			},
+			body: JSON.stringify({
+				response_type: 'in_channel',
+				text: "```\n" + info + "\n```"
+			}),
+			followRedirect: false
+		})
+		.then(res => {
+			process.stdout.write('ok')
+			console.log('ok')
+		})
+		.catch(err => {
+			process.stdout.write(err.message)
+		})
+}
+
+async function quote(req, res) {
+	try {
+		const post = await streamToPromise(req)
+		const data = qs.parse(post.toString('utf8'))
+		await check(data)
+		delayed(data)
+		send(res, 200, "localizando o papel...")
+	} catch (err) {
+		send(res, 400, err.message)
+	}
+}
+
+module.exports = cors(compress(quote))
