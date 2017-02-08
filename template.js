f2c5286adf1c07e97de38cf33012010919ccb44c
Thiago Lagden <lagden@gmail.com>
2017-02-08 20:15:47 -0200

    template zuado do vila

--- template.js
+++ template.js
@@ -1,10 +1,17 @@
 function slackBody(data) {
 	if (data && data.papel) {
+		// return [
+		// 	`*${data.papel}* ${/^-/.test(data.variacao) ? ':_neg:' : ':_pos:'} ${data.variacao} ${data.ultimo}\n`,
+		// 	`:_max: ${data.max} :_min: ${data.min}\n`,
+		// 	`:_open: ${data.abertura} :_close: ${data.fechamento}\n`,
+		// 	`:_vol: ${data.volume} :_time: ${data.hora}`
+		// ].join('')
 		return [
-			`*${data.papel}* ${/^-/.test(data.variacao) ? ':_neg:' : ':_pos:'} ${data.variacao} ${data.ultimo}\n`,
-			`:_max: ${data.max} :_min: ${data.min}\n`,
-			`:_open: ${data.abertura} :_close: ${data.fechamento}\n`,
-			`:_vol: ${data.volume} :_time: ${data.hora}`
+			`*${data.papel}*  :cotacao: ${data.ultimo}`,
+			` ${/^-/.test(data.variacao) ? ':negativo:' : ':positivo:'} ${data.variacao}`,
+			` :max: ${data.max} :min: ${data.min}`,
+			` :abertura: ${data.abertura} :fechamento: ${data.fechamento}`,
+			` :volume: ${data.volume} :hora: ${data.hora}`
 		].join('')
 	}
 	return '✖ Cotação não encontrada'
